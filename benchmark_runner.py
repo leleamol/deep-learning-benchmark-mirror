df8491d75a9ac0e77299f1f21fa23decfb0cc04a
eftiquar <eftiquar@amazon.com>
2018-03-26 17:52:02 -0700

    Adding mterics template argument to benchmarkrunner

--- benchmark_runner.py
+++ benchmark_runner.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--num-gpus', type=int, help='Numbers of gpus. e.g. --num-gpus 8')
     parser.add_argument('--command-to-execute', type=str, help='The script command that performs benchmarking')
     parser.add_argument('--data-set', type=str, help='The data set to use for benchmarking, eg. imagenet')
-    parser.add_argument('--metrics-template', type=str, help='The template file to use for metrics pattern', default =CONFIG_TEMPLATE)
+    parser.add_argument('--metrics-template', type=str, help='The template file to use for metrics pattern', default=CONFIG_TEMPLATE)
     
     args = parser.parse_args()    
    
