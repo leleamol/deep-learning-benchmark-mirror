39cb128ca7bc592a5b3c5637624e2f1b7984c6f0
Rahul <rahulhuilgol@gmail.com>
2018-04-13 14:54:39 -0700

    indent

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -1,25 +1,20 @@
-
 import os
 
-
 def getImagenetData(dataset):
-	if dataset == 'imagenet' or dataset == 'imagenet-256px':
-	    if not os.path.exists('./data/imagenet1k-train.rec'):
-	        os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-train.rec -P data/")
-	    if not os.path.exists('./data/imagenet1k-val.rec'):
-	        os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P data/")
-	elif dataset == 'imagenet-480px':
-		if not os.path.exists('./data/imagenet_train.rec'):
-			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P data/")
-		if not os.path.exists('./data/imagenet_val.rec'):
-			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_val.rec -P data/")
-		if not os.path.exists('./data/imagenet1k-val.rec'):
-	        os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P data/")
-	else:
-		raise ValueError('Unknown dataset')
-
-
-
+    if dataset == 'imagenet' or dataset == 'imagenet-256px':
+        if not os.path.exists('./data/imagenet1k-train.rec'):
+            os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-train.rec -P data/")
+        if not os.path.exists('./data/imagenet1k-val.rec'):
+            os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P data/")
+    elif dataset == 'imagenet-480px':
+        if not os.path.exists('./data/imagenet_train.rec'):
+            os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P data/")
+        if not os.path.exists('./data/imagenet_val.rec'):
+            os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_val.rec -P data/")
+        if not os.path.exists('./data/imagenet1k-val.rec'):
+            os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P data/")
+    else:
+        raise ValueError('Unknown dataset')
 # wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P data/
 # wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_val.rec -P data/
 # cd ~
