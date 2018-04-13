9896c721c5a69b5cdf177ca0e9d3311db18660e8
Rahul <rahulhuilgol@gmail.com>
2018-04-13 12:02:57 -0700

    fix data download

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -10,8 +10,8 @@ def getImagenetData(dataset):
 	        os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P data/")
 	elif dataset == 'imagenet-480px':
 		if not os.path.exists('./data/imagenet_train.rec'):
-			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec")
-		if not os.path.exists('./data/imagenet_train.rec'):
-			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec")
+			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P data/")
+		if not os.path.exists('./data/imagenet_val.rec'):
+			os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_val.rec -P data/")
 	else:
 		raise ValueError('Unknown dataset')
