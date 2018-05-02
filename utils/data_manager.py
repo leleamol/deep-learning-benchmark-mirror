26f0e760ae877573eb7d904ea93a985a1051646c
Rahul <rahulhuilgol@gmail.com>
2018-05-02 14:43:21 -0700

    new option for imagenet data

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -6,11 +6,14 @@ def getImagenetData(dataset):
             os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-train.rec -P ~/data/")
         if not os.path.exists(os.path.expanduser('~/data/imagenet1k-val.rec')):
             os.system("wget -q https://s3.amazonaws.com/ragab-datasets/imagenet2012/imagenet1k-val.rec -P ~/data/")
+    elif dataset == 'imagenet-480px-q95':
+        if not os.path.exists(os.path.expanduser('~/data/imagenet_train.rec')):
+            os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P ~/data/")
+        if not os.path.exists(os.path.expanduser('~/data/val-256px-q95.rec')):
+            os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/256px-95quality-imagenet/val-256px-q95.rec -P ~/data/")
     elif dataset == 'imagenet-480px-256px-q95':
         if not os.path.exists(os.path.expanduser('~/data/imagenet_train.rec')):
             os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_train.rec -P ~/data/")
-        if not os.path.exists(os.path.expanduser('~/data/imagenet_val.rec')):
-            os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/480px-95quality-imagenet/imagenet_val.rec -P ~/data/")
         if not os.path.exists(os.path.expanduser('~/data/train-256px-q95.rec')):
             os.system("wget -q https://s3.amazonaws.com/aws-ml-platform-datasets/256px-95quality-imagenet/train-256px-q95.rec -P ~/data/")
         if not os.path.exists(os.path.expanduser('~/data/val-256px-q95.rec')):
