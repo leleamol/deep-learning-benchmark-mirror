a16df20211e85852243c4a282397b13537a3a2a8
Qing <lanking520@live.com>
2018-06-01 11:43:22 -0700

    add openblas config

--- scala_benchmark/runScala.sh
+++ scala_benchmark/runScala.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y maven
 git clone --recursive https://github.com/apache/incubator-mxnet $HOME/incubator-mxnet
 cp -R scala_benchmark/ $HOME/
 cd $HOME/incubator-mxnet
-make -j4
+make -j $(nprocs) ADD_LDFLAGS += -L/usr/local/lib USE_OPENMP=1 USE_DIST_KVSTORE=1 USE_S3=1 USE_BLAS=openblas
 make scalapkg
 make scalatest
 cd ../scala_benchmark
