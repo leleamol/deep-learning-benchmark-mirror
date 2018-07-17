7108af1e25780a7c76dbd15bef733e6baaa504a5
Jack Mazanec <jmazane@amazon.com>
2018-07-17 15:31:27 -0700

    only runs for python 2

--- sagemaker_testing/pytorch/pytorch-examples-resnet50.sh
+++ sagemaker_testing/pytorch/pytorch-examples-resnet50.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/pytorch/examples.git
 cd examples/imagenet
 pip install -r requirements.txt
-python main.py -a resnet50 ../../../imagenet
+python2 main.py -a resnet50 ../../../imagenet
 
