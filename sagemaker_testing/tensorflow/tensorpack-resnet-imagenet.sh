1e262b52aceac7df369f2a621ba82093a78c488a
Jack Mazanec <jmazane@amazon.com>
2018-07-24 16:26:49 -0700

    changed to use 8 gpus

--- sagemaker_testing/tensorflow/tensorpack-resnet-imagenet.sh
+++ sagemaker_testing/tensorflow/tensorpack-resnet-imagenet.sh
@@ -9,4 +9,4 @@ pip2 install tabulate
 pip2 install opencv-python
 apt-get update -y && apt-get install -y libsm6 libxext6
 apt-get install -y libfontconfig1 libxrender1
-python2 tensorpack/examples/ResNet/imagenet-resnet.py --data ~/imagenet --gpu 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 -d 50 --data_format 'NHWC' --batch 64
+python2 tensorpack/examples/ResNet/imagenet-resnet.py --data ~/imagenet --gpu 0,1,2,3,4,5,6,7 -d 50 --data_format 'NHWC' --batch 64
