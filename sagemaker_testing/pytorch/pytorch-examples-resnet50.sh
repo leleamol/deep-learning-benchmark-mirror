7b038336d0cc32202b30a8c2dd909fbe7f06bd2a
Jack Mazanec <jmazane@amazon.com>
2018-07-26 15:54:24 -0700

    update

--- sagemaker_testing/pytorch/pytorch-examples-resnet50.sh
+++ sagemaker_testing/pytorch/pytorch-examples-resnet50.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/pytorch/examples.git
 cd examples/imagenet
 pip install -r requirements.txt
-python2 main.py -a resnet18 ../../../imagenet --batch-size 128 --epochs 10
+python2 main.py -a resnet50 ../../../imagenet --batch-size 128 --epochs 10
 
