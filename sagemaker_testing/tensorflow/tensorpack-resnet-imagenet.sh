3557d4c35d5ab387272ca40b8b84a56da9c49d3b
Jack Mazanec <jmazane@amazon.com>
2018-07-28 10:28:12 -0700

    changed pip2 to pip

--- sagemaker_testing/tensorflow/tensorpack-resnet-imagenet.sh
+++ sagemaker_testing/tensorflow/tensorpack-resnet-imagenet.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/jmazanec15/tensorpack.git
 cp -r tensorpack/tensorpack tensorpack/examples/ResNet
-pip2 install tqdm
-pip2 install subprocess32
-pip2 install pyarrow
-pip2 install functools32
-pip2 install zmq
-pip2 install tabulate
-pip2 install opencv-python
+pip install tqdm
+pip install subprocess32
+pip install pyarrow
+pip install functools32
+pip install zmq
+pip install tabulate
+pip install opencv-python
 apt-get update -y && apt-get install -y libsm6 libxext6
 apt-get install -y libfontconfig1 libxrender1
-python2 tensorpack/examples/ResNet/imagenet-resnet.py --data ~/imagenet --gpu 0,1,2,3,4,5,6,7 -d 50 --data_format 'NHWC' --batch 64
+python tensorpack/examples/ResNet/imagenet-resnet.py --data ~/imagenet --gpu 0,1,2,3,4,5,6,7 -d 50 --data_format 'NHWC' --batch 64
