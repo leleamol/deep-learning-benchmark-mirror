e63372d8b6a13584a2da3efed80894e54033424b
Jack Mazanec <jmazane@amazon.com>
2018-08-03 10:33:10 -0700

    added pip installation of termcolor

--- conda_dlami_testing/tensorflow/tensorpack-resnet-imagenet.sh
+++ conda_dlami_testing/tensorflow/tensorpack-resnet-imagenet.sh
@@ -7,6 +7,7 @@ pip install functools32
 pip install zmq
 pip install tabulate
 pip install opencv-python
+pip install termcolor
 apt-get update -y && apt-get install -y libsm6 libxext6
 apt-get install -y libfontconfig1 libxrender1
 python tensorpack/examples/ResNet/imagenet-resnet.py --data ~/imagenet --gpu 0,1,2,3,4,5,6,7 -d 50 --data_format 'NHWC' --batch 64
